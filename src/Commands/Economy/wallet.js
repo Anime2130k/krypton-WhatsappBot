3f2ea67120f4e3bc22b3bad879479eb0389c4545
Rishikesh Saikia <115917383+Anime2130k@users.noreply.github.com>
2023-10-05 06:50:13 +0530

    Update wallet.js

--- src/Commands/Economy/wallet.js
+++ src/Commands/Economy/wallet.js
@@ -1,1 +1,15 @@
+module.exports = {
+    name: 'wallet',
+    aliases: ['wal'],
+    category: 'economy',
+    exp: 5,
+    react: "✅",
+    description: 'Shows the wallet value',
+    async execute(client, arg, M) {
+        M.reply(
+            `${(await client.contact.getContact(M.sender, client)).username} | *Wallet:* ${(await client.cradit.get(`${M.sender}.wallet`)) || 0
+            }`
+        )
+    }
+}
 
