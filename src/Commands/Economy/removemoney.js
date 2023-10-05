8d6c6fd7619c0315f80b4c8d633d5842f4b735cc
Rishikesh Saikia <115917383+Anime2130k@users.noreply.github.com>
2023-10-05 06:46:51 +0530

    Update removemoney.js

--- src/Commands/Economy/removemoney.js
+++ src/Commands/Economy/removemoney.js
@@ -1,1 +1,30 @@
-
+module.exports = {
+    name: "removemoney",
+    aliases: ["rm"],
+    category: "economy",
+    exp: 5,
+    react: "✅",
+    description: "Removes money from mentioned user's wallet",
+    usage: "[mention] [amount]",
+    async execute(client, arg, M) {
+      const mentionedUser = M.mentions[0];
+      const amountToRemove = parseInt(arg[1]);
+  
+      if (!mentionedUser || !amountToRemove || isNaN(amountToRemove) || amountToRemove <= 0) {
+        return M.reply("Invalid usage. Please use the command like this:\n\n`!removemoney [mention] [amount]`");
+      }
+  
+      const currentBalance = await client.cradit.get(`${mentionedUser}.wallet`) || 0;
+  
+      if (currentBalance < amountToRemove) {
+        return M.reply(`${(await client.contact.getContact(mentionedUser, client)).username}'s wallet balance is insufficient.`);
+      }
+  
+      const newBalance = currentBalance - amountToRemove;
+  
+      await client.cradit.set(`${mentionedUser}.wallet`, newBalance);
+  
+      M.reply(`${(await client.contact.getContact(mentionedUser, client)).username}'s wallet has been updated. New balance: ${newBalance}`);
+    }
+  };
+  
